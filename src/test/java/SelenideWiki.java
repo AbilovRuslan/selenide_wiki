b64c793f748c51b731ed52357aba3544c9b013f2
Abilov RU <ru_a@mail.ru>
2023-06-06 00:13:24 +0300

    add fix HW

--- src/test/java/SelenideWiki.java
+++ src/test/java/SelenideWiki.java
@@ -1,9 +1,12 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static java.awt.SystemColor.text;
 
 public class SelenideWiki {
     @BeforeAll
@@ -17,8 +20,25 @@ public class SelenideWiki {
 
     }
     @Test
-     void successTest() {
+     void SelenideWiki() {
+        //Откройте  Github
         open ("https://github.com");
-        $("['placeholder='Search GitHub']").setValue("selenide").pressEnter();
+        $("[placeholder='Search GitHub']").setValue("selenide").pressEnter();
+        // Откройте страницу Selenide
+        $$("ul.repo-list").first().$("a").click();
+        //Перейдите в раздел Wiki проекта
+        $("#repository-container-header").$("#wiki-tab").click();;
+
+        //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions. Откройте страницу SoftAssertions
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").click();
+
+        //проверьте что внутри есть пример кода для JUnit5
+        $("#user-content-3-using-junit5-extend-test-class").shouldBe(Condition.visible).scrollTo();
     }
+
+
+    
 }
+
+
